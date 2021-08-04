4c8a178d9035531047683f3fbf3effa688711321
Maleesha Mithun Berugoda <88098373+devteam20212@users.noreply.github.com>
2021-08-04 22:53:50 +0530

    Add files via upload

---
+++ registerd.php
@@ -0,0 +1,32 @@
+<?php
+$topics=$_POST["topics"];
+echo " " .$topics;
+$fname=$_POST["fname"];
+echo " " .$fname;
+$lname=$_POST["lname"];
+echo " " .$lname;
+$sname=$_POST["sname"];
+echo " " .$sname;
+$address=$_POST["address"];
+echo "<br>" .$address;
+$city=$_POST["city"];
+echo "<br>" .$city;
+$country=$_POST["country"];
+echo "<br>" .$country;
+$email=$_POST["email"];
+echo "<br>" .$email;
+$birthday=$_POST["birthday"];
+echo "<br>" .$birthday;
+$gender=$_POST["gender"];
+echo "<br>" .$gender;
+
+
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>You are now Registerd</h1>
+    </body>
+</html>
